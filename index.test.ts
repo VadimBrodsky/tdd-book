2540fd9c277016dc16264755f6f4f970acc4a570
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2024-08-13 22:01:17 -0400

    complete chapter 5

--- index.test.ts
+++ index.test.ts
@@ -1,14 +1,25 @@
-import { expect, test } from "bun:test";
-import { Dollar } from ".";
+import { expect, test, describe } from "bun:test";
+import { Dollar, Franc } from ".";
 
-test("multiplication", () => {
-  let five = new Dollar(5);
+describe("Dollars", () => {
+  test("multiplication", () => {
+    let five = new Dollar(5);
 
-  expect(five.times(2)).toEqual(new Dollar(10));
-  expect(five.times(3)).toEqual(new Dollar(15));
+    expect(five.times(2)).toEqual(new Dollar(10));
+    expect(five.times(3)).toEqual(new Dollar(15));
+  });
+
+  test("equiality", () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTrue();
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalse();
+  });
 });
 
-test("equiality", () => {
-  expect(new Dollar(5).equals(new Dollar(5))).toBeTrue();
-  expect(new Dollar(5).equals(new Dollar(6))).toBeFalse();
+describe("Francs", () => {
+  test("multiplication", () => {
+    let five = new Franc(5);
+
+    expect(five.times(2)).toEqual(new Franc(10));
+    expect(five.times(3)).toEqual(new Franc(15));
+  });
 });
