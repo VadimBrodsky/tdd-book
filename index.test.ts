8b71ec65171111d46c09f94577c4367aa2ed27a5
Vadim Brodsky <vadim.brodsky@coinbase.com>
2024-09-12 12:38:01 -0400

    add a currency method to Franc and Dollar

--- index.test.ts
+++ index.test.ts
@@ -30,3 +30,8 @@ describe("Francs", () => {
     expect(Money.franc(5).equals(Money.dollar(5))).toBeFalse();
   });
 });
+
+test("Currency", () => {
+  expect(Money.dollar(1).currency()).toBe("USD");
+  expect(Money.franc(1).currency()).toBe("CHF");
+});
--- index.ts
+++ index.ts
@@ -9,33 +9,48 @@ export abstract class Money {
     return this.amount == object.amount && this.constructor.name === object.constructor.name;
   }
 
-  abstract times(multiplier: number): Money
+  abstract times(multiplier: number): Money;
+  abstract currency(): String;
 
   public static dollar(amount: number): Dollar {
     return new Dollar(amount);
   }
 
-  public static franc(amount: number): Dollar {
+  public static franc(amount: number): Franc {
     return new Franc(amount);
   }
 }
 
 export class Dollar extends Money {
+  private currencySymbol: string;
+
   constructor(amount: number) {
     super(amount);
+    this.currencySymbol = "USD";
   }
 
   times(multiplier: number): Money {
     return new Dollar(this.amount * multiplier);
   }
+
+  currency() {
+    return this.currencySymbol;
+  }
 }
 
 export class Franc extends Money {
+  private currencySymbol: string;
+
   constructor(amount: number) {
     super(amount);
+    this.currencySymbol = "CHF";
   }
 
   times(multiplier: number): Money {
     return new Franc(this.amount * multiplier);
+  }
+
+  currency() {
+    return this.currencySymbol;
   }
 }
